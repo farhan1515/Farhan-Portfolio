8b4a66257e94410172d181f85634cb33d4caa0d0
farhan1515 <farhananwar784@gmail.com>
2024-10-11 23:36:40 +0530

    new style

--- script.js
+++ script.js
@@ -1,95 +1,104 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        //  sticky navbar on scroll script  //
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
-        }
+  $(window).scroll(function () {
+    //  sticky navbar on scroll script  //
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-        //  scroll-up button show/hide script  //
-        if (this.scrollY > 500) {
-            $(".scroll-up-btn").addClass("show");
-        } else {
-            $(".scroll-up-btn").removeClass("show");
-        }
-    });
+    //  scroll-up button show/hide script  //
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    //  slide-up script  //
+  //  slide-up script  //
 
-    $(".scroll-up-btn").click(function () {
-        $("html").animate({ scrollTop: 0 });
-        //  removing smooth scroll on slide-up button click  //
-        $("html").css("scrollBehavior", "auto");
-    });
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    //  removing smooth scroll on slide-up button click  //
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    $(".navbar .menu li a").click(function () {
-        //  Smooth scroll on Menu Items click  //
+  $(".navbar .menu li a").click(function () {
+    //  Smooth scroll on Menu Items click  //
 
-        $("html").css("scrollBehavior", "smooth");
-    });
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    //  Toggle Navbar  //
+  //  Toggle Navbar  //
 
-    $(".menu-btn").click(function () {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-    });
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    //  Typing Text Animation  //
+  //  Typing Text Animation  //
 
-    var typed = new Typed(".typing", {
-        strings: [
-            "WEB Developer",
-            "Team Player",
-            "Continuously Learning"
-            // "Software Developer",
-            // "Python Developer",
-            //"Founder",
-            // "Author"
-        ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  var typed = new Typed(".typing", {
+    strings: [
+      "Flutter Developer",
+      "Web Developer",
+      "Team player",
+      "Continuously Learning",
+      // "Software Developer",
+      // "Python Developer",
+      //"Founder",
+      // "Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: [
-            "WEB Developer",
-            "Team Player",
-            "Continuously Learning",
-            //"Fullstack Developer",
-            //"Software Developer",
-            //"Python Developer",
-            //"Founder",
-            //"Author"
-        ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Flutter Developer",
+      "WEB Developer",
+      "Team Player",
+      "Continuously Learning",
+      //"Fullstack Developer",
+      //"Software Developer",
+      //"Python Developer",
+      //"Founder",
+      //"Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    //  Owl Carousel  //
+  //  Owl Carousel  //
 
-    $(".carousel").owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+document.querySelector(".btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#contact").scrollIntoView({
+    behavior: "smooth",
+  });
 });
